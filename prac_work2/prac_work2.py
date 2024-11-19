fef01179c8228af2c25b0f82fb24668529908848
pablo <aknelsaltikov2006@gmail.com>
2024-11-19 09:08:45 +0200

    Update prac_work2.py

--- prac_work2/prac_work2.py
+++ prac_work2/prac_work2.py
@@ -3,39 +3,85 @@ from bs4 import BeautifulSoup
 from threading import *
 from time import time, sleep
 
-def parse_weather():
+
+def weather():
     url = 'https://meteofor.com.ua/'
     req = urllib.request.Request (
         url,
         headers= {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
         'Chrome/91.0.4472.124 Safari/537.36'}
     )
-
-    try:
-        with urllib.request.urlopen(req) as response:
-            html = response.read()
-            soup = BeautifulSoup(html, 'html.parser')
-        #print(html)
-        #temp_elms = soup.find_all('value')
-        #temp_elms = soup.find_all('div', class_='temperature')
-        temp_elms = soup.find_all('span', {'class': 'temp-value'})
-
-
-        if not temp_elms:
-            print("No temperature data found.")
-            return
-        for temp in temp_elms:
-            temp_val = temp.get('value')
-            #temp_txt = temp.text
-        print(f"Temperature: {temp_val}")
-        return int(temp_val)
-
-    except urllib.error.HTTPError as e:
-        print(f"HTTP Error: {e.code}")
-    except urllib.error.URLError as e:
-        print(f"URL Error: {e.reason}")
-    except Exception as e:
-        print(f"Error: {e}")
+    with urllib.request.urlopen(req) as file:
+        src = file.read()
+    
+    soup = BeautifulSoup(src, 'html.parser')
+    
+    temperature_cont = soup.find("div", class_="temperature")
+    if temperature_cont:
+        temperature = temperature_cont.find("temperature-value")
+        if temperature:
+            value = temperature.get("value")
+            print(f"Temperature: {value}°C")
+        else:
+            print("teg temperature-value not found")
+    else:
+        print("not fount el with class temperature")
 
 
-parse_weather()
+def usd():
+    url = "https://minfin.com.ua/ua/currency/"
+    req = urllib.request.Request(
+        url,
+        headers= {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Chrome/91.0.4472.124 Safari/537.36'}
+    )
+    with urllib.request.urlopen(req) as file:
+        src = file.read()
+
+    soup = BeautifulSoup(src, "html.parser")
+
+    usd = soup.find("div", class_="sc-1x32wa2-9 bKmKjX")
+    if usd:
+        print(f"Dollar exchange rate: {usd.text[:5]}")
+    else:
+        print("Dollar exchange rate not found")
+    
+def Gwei():
+    url = "https://etherscan.io/gastracker"
+    req = urllib.request.Request(
+        url,
+        headers= {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Chrome/91.0.4472.124 Safari/537.36'}
+    )
+    with urllib.request.urlopen(req) as file:
+        src = file.read()
+
+    soup = BeautifulSoup(src, "html.parser")
+
+    gwei = soup.find("div", class_="h4 text-danger mb-0").find("span")
+    if gwei:
+        print(f"Gwei in ETH: {gwei.text.strip()}")
+    else:
+        print("Gwei in ETH not found")
+
+
+def avr_time(function, repeats = 5):
+    in_all_time = 0
+    for i in range(repeats):
+        start_time = time()
+        function()
+        end_time = time()
+        in_all_time += (end_time - start_time)
+    return in_all_time / repeats
+
+
+print("Sequential execution:")
+gwei_time = avr_time(Gwei)
+usd_time = avr_time(usd)
+weather_time = avr_time(weather)
+print(f"Average gwei time: {gwei_time} sec")
+print(f"Average weather time: {weather_time} sec")
+print(f"Average usd time: {usd_time} sec")
+
+
+print("\n\nParallel execution:")
