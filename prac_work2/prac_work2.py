a70fcdb43e4528d97c8725ad9de32991a78cc7b8
pablo <aknelsaltikov2006@gmail.com>
2024-11-19 09:10:04 +0200

    Update prac_work2.py

--- prac_work2/prac_work2.py
+++ prac_work2/prac_work2.py
@@ -1,6 +1,6 @@
 import urllib.request
 from bs4 import BeautifulSoup
-from threading import *
+import threading
 from time import time, sleep
 
 
@@ -82,6 +82,26 @@ weather_time = avr_time(weather)
 print(f"Average gwei time: {gwei_time} sec")
 print(f"Average weather time: {weather_time} sec")
 print(f"Average usd time: {usd_time} sec")
+print(f"All time in sequential: {gwei_time + usd_time + weather_time} sec")
+
+
+def parallel_time(functions, repeats = 5):
+    in_all_time = 0
+    for i in range(repeats):
+        threads = []
+        start_time = time()
+        for function in functions:
+            thread = threading.Thread(target = function)
+            threads.append(thread)
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+        end_time = time()
+        in_all_time += (end_time - start_time)
+    return in_all_time / repeats
 
 
 print("\n\nParallel execution:")
+in_all_parallel_time = parallel_time([Gwei, usd, weather])
+print(f'All time in parallel: {in_all_parallel_time} sec')
